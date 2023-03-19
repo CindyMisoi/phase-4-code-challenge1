32eb058507794e97326179013894f2c9d8215eb6
CindyMisoi <crmisoi2k@gmail.com>
2023-03-20 00:11:36 +0300

    added routes and controller methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,22 @@
 class RestaurantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def index
+        restaurants = Restaurant.all
+        render json: restaurants
+    end
+    def show
+        restaurant = Restaurant.find(params[:id])
+        render json: restaurant
+    end
+    def destroy
+        restaurant = Restaurant.find(params[:id])
+        restaurant.destroy
+        render json: restaurant
+        head :no_content
+    end
+
+    # private => strong params
+    def render_not_found_response
+        render json: {error: "Restaurant not found"}, status: :not_found
+    end
 end
